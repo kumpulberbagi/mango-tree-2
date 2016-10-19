1a9abc4060c8359e8b9be7c4c2fe4b833abd394c
Alessandro <Alessandro.christopher@gmail.com>
2016-10-20 00:24:41 +0700

    Submitted

--- mango_tree_2.js
+++ mango_tree_2.js
@@ -1,12 +1,99 @@
-"use strict"
-
-// Release 0
-class AppleTree {}
-class Apple {}
+class AppleTree {
+  constructor() {
+      allTree.allFruits.push(this)
+        this.height = 10
+        this.age = 10
+        this.fruits = "Apel"
+        this.deadFruit = true;
+  }
+}
+class Apple extends AppleTree{
+  constructor()
+  {
+    super()
+  }
+}
 
 // Release 1
-class FruitTree {}
-class Fruit {}
+class FruitTree {
+  constructor()
+  {
+    allTree.allFruits.push(this)
+    this.height = 10;
+    this.age = 4
+    this.fruits = "Fruit"
 
-// Release 2
-class TreeGrove {}
+  }
+}
+class Fruit extends FruitTree{
+  constructor()
+  {
+
+    super()
+  }
+}
+class PearTree{
+  constructor()
+  {
+    allTree.allFruits.push(this)
+    this.height = 100
+    this.age = 20
+    this.fruits = "Pear"
+    this.berbuah = true;
+  }
+}
+class Pear extends PearTree
+{
+  constructor()
+  {
+    super()
+
+  }
+}
+
+
+
+class TreeGrove {
+    constructor()
+    {
+      this.allFruits = []
+    }
+
+    age()
+    {
+        console.log("Apple's age : "+apple.age+" Pear's age : "+ pear.age)
+    }
+
+    trees()
+    {
+        console.log("Jenis Pohon : "+apple.fruits+" "+pear.fruits)
+    }
+
+    mature_trees()
+    {
+      if(pear.berbuah)
+      {
+        console.log("Pohon yang berbuah "+pear.fruits)
+      }
+    }
+
+
+    dead_trees()
+    {
+      if(apple.deadFruit)
+      {
+        console.log("Pohon yang mati "+apple.fruits)
+      }
+    }
+}
+
+
+
+var allTree = new TreeGrove()
+var apple = new AppleTree()
+var pear = new PearTree()
+console.log(allTree)
+allTree.age()
+allTree.trees()
+allTree.mature_trees()
+allTree.dead_trees()
